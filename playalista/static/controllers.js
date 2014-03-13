713d3a9e7f537f1a5b45c5bd3cdbc7ee14b6a237
Peter Gao <pgao@airbears2-10-142-32-107.airbears2.1918.berkeley.edu>
2014-03-12 23:00:21 -0700

    more default song

--- playalista/static/controllers.js
+++ playalista/static/controllers.js
@@ -5,9 +5,143 @@ var YOUTUBE_API_KEY = 'AIzaSyCiERdaxIuw6WOxFFB1joOu1XBEuuCO2dg';
 var YOUTUBE_API_URL = 'https://www.googleapis.com/youtube/v3/search'
 
 var DEFAULT_SONGS = [
-  'Epistrophy - Thelonious Monk',
-  'Blitzkrieg Bop - The Ramones',
-  'Derezzed (Remixed By The Glitch Mob) - Daft Punk'
+  "Epistrophy - Thelonious Monk",
+  "Blitzkrieg Bop - The Ramones",
+  "Hearts Grow (kasanaru kage)",
+  "Danger - 11h30",
+  "Erroll Garner - Autumn Leaves",
+  "Bad Boy Good Man",
+  "Virtual Boy - Mass",
+  "Born Gold - Lawn Knives",
+  "Phoenix - 1901",
+  "Dance yourself clean",
+  "Royksopp forever",
+  "An Astrologists Guide To The Stars In The Sky - Biorheology",
+  "Hall of the Mountain King",
+  "Stan Getz Desafinado",
+  "Handsome boy modeling school",
+  "Miami showdown",
+  "New Slang",
+  "Beirut - Nantes",
+  "Tycho - A walk",
+  "Ghostwriter",
+  "Andrew Shum",
+  "Mr. Sandman chordettes",
+  "Bassnectar lights",
+  "life/universe detektivbyran",
+  "Stayin Alive bee gees",
+  "Institutionalized suicidal tendencies",
+  "Sunset Song",
+  "fragrance of Dark Coffee",
+  "Eyes on fire, Zed's Dead",
+  "First Snow - Emancipator",
+  "m-taku, One",
+  "mala, alicia",
+  "Kiss with a fist",
+  "Rose cornandbeans",
+  "Russian opera",
+  "Floppy disc unatco",
+  "Doctor who tesla",
+  "Unatco old",
+  "Unatco new",
+  "Comin home baby",
+  "No one knows my plan",
+  "2wicky",
+  "Yukar",
+  "Rearrange miles kane",
+  "Booty swing",
+  "Session linkin park",
+  "Sail",
+  "What you know",
+  "Takenobu",
+  "Korra music",
+  "Danzon 2",
+  "The Mating Game",
+  "Dirty Laundry",
+  "24 Hours open",
+  "Tank cowboy bebop",
+  "I will survive",
+  "Shooting stars bag raider",
+  "Fuck shit stack",
+  "I'm Just Your Problem",
+  "Daddy why did you eat my Fries",
+  "Positive Black Ignorance",
+  "George Washington song",
+  "Appeal to Timbaland",
+  "Crystalised Remix",
+  "heyyeyaaeyaaaeyaeyaa",
+  "Schlozer",
+  "Moanin by Mingus",
+  "Moanin by 7note",
+  "Eldar moanin kennedy center",
+  "Ray Brown master class",
+  "Scrap IO neotokyo",
+  "Tin Soldiers neotokyo",
+  "Impetus neotokyo",
+  "Annul neotokyo",
+  "Footprint neotokyo",
+  "Monk Quartet",
+  "Sail remix",
+  "Quantic Transatlantic",
+  "I miss you ayur",
+  "Flower dance",
+  "Round midnight 7note",
+  "Reflection eternal",
+  "Kronos and ron carter",
+  "Magnum Force theme",
+  "Black orpheus",
+  "stanley clarke",
+  "Moanin art blakey",
+  "Moanin eldar",
+  "Polyushka Polye",
+  "Monday remix",
+  "Derezzed remix",
+  "Where is my mind",
+  "Campus vampire weekend",
+  "M79 vampire weekend",
+  "Clint Eastwood gorillaz",
+  "Cheers Elephant",
+  "Murderers frusciante",
+  "Warrior concerto",
+  "Somebody that I used to know dubstep",
+  "Somebody told me",
+  "miss Otis regrets",
+  "Is this it the strokes",
+  "God only knows bioshock",
+  "Fortunate son",
+  "Ex-boyfriend",
+  "Busker adam ben ezra",
+  "Tom Waits down in the hole",
+  "Blind boys down in the hole",
+  "Get Lucky George Barnett",
+  "All caps madvillain",
+  "Seven nation army",
+  "Swisgaar's solo",
+  "Feels like we only go backwards",
+  "Tycho - A Walk",
+  "Home Stay",
+  "Smirnoff Tea Party",
+  "Farewell Spaceman",
+  "Zero 7",
+  "Space Walk lemon jelly",
+  "Everything is OK halou",
+  "Baroque in Rhythm",
+  "ante up",
+  "i go i go i go wave machine",
+  "spitfire porter robinson",
+  "charlie brown theme",
+  "charlie brown jazz",
+  "i always kill the things i love",
+  "unchain my heart hugh laurie",
+  "yesterdays paul chambers",
+  "circle be unbroken",
+  "step vampire weekend",
+  "death note l's theme",
+  "torched song",
+  "when i'm small",
+  "I can see it in your face",
+  "red army choir",
+  "move bitch ludacris",
 ]
 
 var time_out_id = 0;
@@ -94,7 +228,15 @@ playalista.controller('musicCtrl', function($scope, $http) {
   $scope.getVideo = function(searchTerm) {
     var requestUrl = YOUTUBE_API_URL + "?part=id&q=" + encodeURIComponent(searchTerm) + '&order=relevance&type=video&key=' + YOUTUBE_API_KEY;
     $http.get(requestUrl).success(function(data, status, headers, config) {
-      $scope.code = data.items[0].id.videoId;
+      for (var i = 0; i < data.items.length; i++) {
+        if ($scope.history.indexOf(data.items[i].id.videoId) == -1) {
+          $scope.code = data.items[i].id.videoId;
+          break;
+        }
+        console.log("duplicate video found");
+      }
+
+      $scope.history.push($scope.code);
       $scope.getNextVideo();
     }).error(function() {
       console.log("echonest query failed in getVideo");
@@ -124,9 +266,13 @@ playalista.controller('musicCtrl', function($scope, $http) {
 
   $scope.selected = DEFAULT_SONGS[Math.floor(Math.random() * DEFAULT_SONGS.length)];
 
+  console.log('current selected: ' + $scope.selected);
+
   $scope.code = $scope.getVideo($scope.selected);
 
   $scope.next = '';
 
   $scope.suggestions = [];
+
+  $scope.history = []
 });

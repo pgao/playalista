3def26b7f7ca3af2cf904e14faf6dcb54676f77b
Peter Gao <pgao@airbears2-10-142-35-29.airbears2.1918.berkeley.edu>
2014-04-27 16:17:57 -0700

    change titles upon skip and random, responsive height resizing of video, reformatting

--- playalista/static/controllers.js
+++ playalista/static/controllers.js
@@ -5,296 +5,315 @@ var YOUTUBE_API_KEY = 'AIzaSyCiERdaxIuw6WOxFFB1joOu1XBEuuCO2dg';
 var YOUTUBE_API_URL = 'https://www.googleapis.com/youtube/v3/search'
 
 var DEFAULT_SONGS = [
-  "Epistrophy - Thelonious Monk",
-  "Blitzkrieg Bop - The Ramones",
-  "Hearts Grow (kasanaru kage)",
-  "Danger - 11h30",
-  "Erroll Garner - Autumn Leaves",
-  "Bad Boy Good Man",
-  "Virtual Boy - Mass",
-  "Born Gold - Lawn Knives",
-  "Phoenix - 1901",
-  "Dance yourself clean",
-  "Royksopp forever",
-  "An Astrologists Guide To The Stars In The Sky - Biorheology",
-  "Hall of the Mountain King",
-  "Stan Getz Desafinado",
-  "Handsome boy modeling school",
-  "Miami showdown",
-  "New Slang",
-  "Beirut - Nantes",
-  "Tycho - A walk",
-  "Ghostwriter",
-  "Andrew Shum",
-  "Mr. Sandman chordettes",
-  "Bassnectar lights",
-  "life/universe detektivbyran",
-  "Stayin Alive bee gees",
-  "Institutionalized suicidal tendencies",
-  "Sunset Song",
-  "fragrance of Dark Coffee",
-  "Eyes on fire, Zed's Dead",
-  "First Snow - Emancipator",
-  "m-taku, One",
-  "mala, alicia",
-  "Kiss with a fist",
-  "Rose cornandbeans",
-  "Russian opera",
-  "Floppy disc unatco",
-  "Doctor who tesla",
-  "Unatco old",
-  "Unatco new",
-  "Comin home baby",
-  "No one knows my plan",
-  "2wicky",
-  "Yukar",
-  "Rearrange miles kane",
-  "Booty swing",
-  "Session linkin park",
-  "Sail",
-  "What you know",
-  "Takenobu",
-  "Korra music",
-  "Danzon 2",
-  "The Mating Game",
-  "Dirty Laundry",
-  "24 Hours open",
-  "Tank cowboy bebop",
-  "I will survive",
-  "Shooting stars bag raider",
-  "Fuck shit stack",
-  "I'm Just Your Problem",
-  "Daddy why did you eat my Fries",
-  "Positive Black Ignorance",
-  "George Washington song",
-  "Appeal to Timbaland",
-  "Crystalised Remix",
-  "heyyeyaaeyaaaeyaeyaa",
-  "Schlozer",
-  "Moanin by Mingus",
-  "Moanin by 7note",
-  "Eldar moanin kennedy center",
-  "Ray Brown master class",
-  "Scrap IO neotokyo",
-  "Tin Soldiers neotokyo",
-  "Impetus neotokyo",
-  "Annul neotokyo",
-  "Footprint neotokyo",
-  "Monk Quartet",
-  "Sail remix",
-  "Quantic Transatlantic",
-  "I miss you ayur",
-  "Flower dance",
-  "Round midnight 7note",
-  "Reflection eternal nujabes",
-  "Kronos and ron carter",
-  "Magnum Force theme",
-  "Black orpheus",
-  "stanley clarke",
-  "Moanin art blakey",
-  "Moanin eldar",
-  "Polyushka Polye",
-  "Monday remix",
-  "Derezzed remix",
-  "Where is my mind",
-  "Campus vampire weekend",
-  "M79 vampire weekend",
-  "Clint Eastwood gorillaz",
-  "Cheers Elephant",
-  "Murderers frusciante",
-  "Warrior concerto",
-  "Somebody that I used to know dubstep",
-  "Somebody told me",
-  "miss Otis regrets",
-  "Is this it the strokes",
-  "God only knows bioshock",
-  "Fortunate son",
-  "Ex-boyfriend",
-  "Busker adam ben ezra",
-  "Tom Waits down in the hole",
-  "Blind boys down in the hole",
-  "Get Lucky George Barnett",
-  "All caps madvillain",
-  "Seven nation army",
-  "Swisgaar's solo",
-  "Feels like we only go backwards",
-  "Tycho - A Walk",
-  "Home Stay ghost in the shell",
-  "Smirnoff Tea Party",
-  "Farewell Spaceman",
-  "Zero 7",
-  "Space Walk lemon jelly",
-  "Everything is OK halou",
-  "Baroque in Rhythm",
-  "ante up",
-  "i go i go i go wave machine",
-  "spitfire porter robinson",
-  "charlie brown theme",
-  "charlie brown jazz",
-  "i always kill the things i love",
-  "unchain my heart hugh laurie",
-  "yesterdays paul chambers",
-  "circle be unbroken",
-  "step vampire weekend",
-  "death note l's theme",
-  "torched song",
-  "when i'm small",
-  "I can see it in your face",
-  "red army choir",
-  "move bitch ludacris",
-  "come together beatles",
+    "Epistrophy - Thelonious Monk",
+    "Blitzkrieg Bop - The Ramones",
+    "Hearts Grow (kasanaru kage)",
+    "Danger - 11h30",
+    "Erroll Garner - Autumn Leaves",
+    "Bad Boy Good Man",
+    "Virtual Boy - Mass",
+    "Born Gold - Lawn Knives",
+    "Phoenix - 1901",
+    "Dance yourself clean",
+    "Royksopp forever",
+    "An Astrologists Guide To The Stars In The Sky - Biorheology",
+    "Hall of the Mountain King death cello",
+    "Stan Getz Desafinado",
+    "the truth Handsome boy modeling school",
+    "Miami showdown",
+    "New Slang",
+    "Beirut - Nantes",
+    "Tycho - A walk",
+    "Ghostwriter",
+    "Andrew Shum",
+    "Mr. Sandman chordettes",
+    "Bassnectar lights",
+    "life/universe detektivbyran",
+    "Stayin Alive bee gees",
+    "Institutionalized suicidal tendencies",
+    "Sunset Song",
+    "fragrance of Dark Coffee",
+    "Eyes on fire, Zed's Dead",
+    "First Snow - Emancipator",
+    "m-taku, One",
+    "mala, alicia",
+    "Kiss with a fist",
+    "Rose cornandbeans",
+    "Russian opera",
+    "Floppy disc unatco",
+    "Doctor who tesla",
+    "deus ex unatco",
+    "Unatco theme new",
+    "Comin home baby",
+    "No one knows my plan",
+    "2wicky",
+    "Yukar",
+    "Rearrange miles kane",
+    "Booty swing",
+    "Session linkin park",
+    "Sail",
+    "What you know",
+    "Takenobu",
+    "Korra music",
+    "Danzon 2",
+    "The Mating Game",
+    "Dirty Laundry bitter sweet",
+    "24 Hours open",
+    "Tank cowboy bebop",
+    "I will survive",
+    "i will survive igudesman and joo",
+    "Shooting stars bag raider",
+    "Fuck shit stack",
+    "I'm Just Your Problem",
+    "Daddy why did you eat my Fries",
+    "Positive Black Ignorance",
+    "George Washington song",
+    "Appeal to Timbaland",
+    "gorillaz Crystalised Remix",
+    "heyyeyaaeyaaaeyaeyaa",
+    "Schlozer",
+    "Moanin by Mingus",
+    "Moanin by 7note",
+    "Eldar moanin kennedy center",
+    "Ray Brown master class",
+    "Scrap IO neotokyo",
+    "Tin Soldiers neotokyo",
+    "Impetus neotokyo",
+    "Annul neotokyo",
+    "Footprint neotokyo",
+    "Monk Quartet",
+    "Sail remix",
+    "Quantic Transatlantic",
+    "I miss you ayur",
+    "Flower dance",
+    "Round midnight 7note",
+    "Reflection eternal nujabes",
+    "Kronos and ron carter",
+    "Magnum Force theme",
+    "Black orpheus ray brown",
+    "stanley clarke",
+    "Moanin art blakey",
+    "Moanin eldar",
+    "Polyushka Polye",
+    "Monday remix",
+    "Derezzed remix",
+    "Where is my mind",
+    "Campus vampire weekend",
+    "M79 vampire weekend",
+    "Clint Eastwood gorillaz",
+    "Cheers Elephant leaves",
+    "Murderers frusciante",
+    "Warrior concerto",
+    "Somebody that I used to know dubstep",
+    "Somebody told me",
+    "miss Otis regrets",
+    "Is this it the strokes",
+    "God only knows bioshock",
+    "Fortunate son",
+    "Ex-boyfriend",
+    "Busker adam ben ezra",
+    "Tom Waits down in the hole",
+    "Blind boys down in the hole",
+    "Get Lucky George Barnett",
+    "All caps madvillain",
+    "Seven nation army",
+    "Swisgaar's solo",
+    "Feels like we only go backwards",
+    "Tycho - A Walk",
+    "Home Stay ghost in the shell",
+    "Smirnoff Tea Party",
+    "Farewell Spaceman",
+    "Zero 7",
+    "Space Walk lemon jelly",
+    "Everything is OK halou",
+    "Baroque in Rhythm",
+    "ante up",
+    "i go i go i go wave machine",
+    "spitfire porter robinson",
+    "charlie brown theme",
+    "charlie brown jazz",
+    "i always kill the things i love",
+    "unchain my heart hugh laurie",
+    "yesterdays paul chambers",
+    "circle be unbroken bioshock",
+    "step vampire weekend",
+    "death note l's theme",
+    "torched song",
+    "when i'm small",
+    "I can see it in your face",
+    "red army choir",
+    "move bitch ludacris",
+    "come together beatles",
+    "hope of a favourable outcome official video",
+    "kansas smitty's",
+    "jenny and the plants",
+    "sweater weather the neighborhood",
+    "the a la menthe la caution",
+    "alive 2007 daft punk",
 ]
 
-var time_out_id = 0;
+var timeOutId = 0;
 
 var playalista = angular.module('playalista', ['autocomplete', 'youtube']);
 
-playalista.controller('musicCtrl', function($scope, $http) {
-  $scope.queryEchonest = function(requestType, requestFunction, parameters, callback) {
-    if (!requestType in ['artist', 'song', 'playlist']) {
-      throw exception;
-    }
-    parameters['api_key'] = ECHONEST_API_KEY;
-
-    var requestUrl = [ECHONEST_API_URL, requestType, requestFunction].join('/');
-
-    var paramKeys = Object.keys(parameters);
-    var paramStrings = [];
-
-    for (key in parameters) {
-      if (parameters[key] instanceof Array) {
-        for (var j = 0; j < parameters[key].length; j++) {
-          paramStrings = paramStrings.concat(encodeURIComponent(key) + '=' + encodeURIComponent(parameters[key][j]));
+playalista.controller('musicCtrl', function ($scope, $http) {
+    $scope.queryEchonest = function (requestType, requestFunction, parameters, callback) {
+        if (!requestType in ['artist', 'song', 'playlist']) {
+            throw exception;
         }
-      }
-      else {
-        paramStrings = paramStrings.concat(encodeURIComponent(key) + '=' + encodeURIComponent(parameters[key]));
-      }
-    }
+        parameters['api_key'] = ECHONEST_API_KEY;
 
-    requestUrl = requestUrl.concat('?', paramStrings.join('&'));
+        var requestUrl = [ECHONEST_API_URL, requestType, requestFunction].join('/');
 
-    $http.get(requestUrl).success(callback).error(function() {
-      console.log("echonest query failed in queryEchonest");
-    });
-  };
+        var paramKeys = Object.keys(parameters);
+        var paramStrings = [];
 
-  $scope.getSong = function(searchTerm) {
-    // parameters = {
-    //   'combined': searchTerm,
-    //   'bucket': ['audio_summary'],
-    //   // 'sort': 'artist_familiarity-desc',
-    // };
+        for (key in parameters) {
+            if (parameters[key] instanceof Array) {
+                for (var j = 0; j < parameters[key].length; j++) {
+                    paramStrings = paramStrings.concat(encodeURIComponent(key) + '=' + encodeURIComponent(parameters[key][j]));
+                }
+            } else {
+                paramStrings = paramStrings.concat(encodeURIComponent(key) + '=' + encodeURIComponent(parameters[key]));
+            }
+        }
 
-    // console.log("query: " + searchTerm);
+        requestUrl = requestUrl.concat('?', paramStrings.join('&'));
 
-    // if (searchTerm) {
-    //   if(time_out_id != 0) {
-    //     clearTimeout(time_out_id);
-    //   }
-    //   time_out_id = setTimeout(function() {
-    //     time_out_id = 0;
-    //     $scope.queryEchonest('song', 'search', parameters, function(data, status, headers, config) {
-    //       console.log(data);
-    //       $scope.suggestions = [];
-    //       for (var i = 0; i < data.response.songs.length; i++) {
-    //         var song = data.response.songs[i];
-    //         var entry = song.title + ' - ' + song.artist_name;
-    //         if ($scope.suggestions.indexOf(entry) == -1) {
-    //           $scope.suggestions.push(entry);
-    //         }
-    //       }
-    //     });
-    //   }, 150);
-
-    // }
-
-    var requestUrl = YOUTUBE_API_URL + "?part=snippet&maxResults=10&q=" + encodeURIComponent(searchTerm) + '&order=relevance&type=video&key=' + YOUTUBE_API_KEY;
-
-    if (searchTerm) {
-      if(time_out_id != 0) {
-        clearTimeout(time_out_id);
-      }
-      time_out_id = setTimeout(function() {
-        $http.get(requestUrl).success(function(data, status, headers, config) {
-          $scope.suggestions = [];
-          for (var i = 0; i < data.items.length; i++) {
-            console.log(data.items[i].snippet.title);
-            $scope.suggestions.push(data.items[i].snippet.title);
-          }
-        }).error(function() {
-          console.log("echonest query failed in getVideo");
+        $http.get(requestUrl).success(callback).error(function () {
+            console.log("echonest query failed in queryEchonest");
         });
-      }, 150);
-    }
-  };
-
-  $scope.getRelatedSong = function(songId) {
-    parameters = {
-      'song_id': songId,
-      'format': 'json',
-      'results': '20',
-      'type': 'song-radio',
     };
 
-    if (searchTerm) {
-      // $scope.queryEchonest('playlist', 'static', parameters, function(data, status, headers, config) {
-      //     debugger;
-      // });
-      console.log('getting related song');
-    }
-  };
+    $scope.getSong = function (searchTerm) {
+        // parameters = {
+        //   'combined': searchTerm,
+        //   'bucket': ['audio_summary'],
+        //   // 'sort': 'artist_familiarity-desc',
+        // };
 
-  $scope.getVideo = function(searchTerm) {
-    var requestUrl = YOUTUBE_API_URL + "?part=id&q=" + encodeURIComponent(searchTerm) + '&order=relevance&type=video&key=' + YOUTUBE_API_KEY;
-    $scope.selected = searchTerm;
-    $http.get(requestUrl).success(function(data, status, headers, config) {
-      for (var i = 0; i < data.items.length; i++) {
-        if ($scope.history.indexOf(data.items[i].id.videoId) == -1) {
-          $scope.code = data.items[i].id.videoId;
-          break;
+        // console.log("query: " + searchTerm);
+
+        // if (searchTerm) {
+        //   if(timeOutId != 0) {
+        //     clearTimeout(timeOutId);
+        //   }
+        //   timeOutId = setTimeout(function() {
+        //     timeOutId = 0;
+        //     $scope.queryEchonest('song', 'search', parameters, function(data, status, headers, config) {
+        //       console.log(data);
+        //       $scope.suggestions = [];
+        //       for (var i = 0; i < data.response.songs.length; i++) {
+        //         var song = data.response.songs[i];
+        //         var entry = song.title + ' - ' + song.artist_name;
+        //         if ($scope.suggestions.indexOf(entry) == -1) {
+        //           $scope.suggestions.push(entry);
+        //         }
+        //       }
+        //     });
+        //   }, 150);
+
+        // }
+
+        var requestUrl = YOUTUBE_API_URL + "?part=snippet&maxResults=10&q=" + encodeURIComponent(searchTerm) + 
+            '&videoEmbeddable=true&order=relevance&type=video&key=' + YOUTUBE_API_KEY;
+
+        if (searchTerm) {
+            if (timeOutId != 0) {
+                clearTimeout(timeOutId);
+            }
+            timeOutId = setTimeout(function () {
+                $http.get(requestUrl).success(function (data, status, headers, config) {
+                    $scope.suggestions = [];
+                    for (var i = 0; i < data.items.length; i++) {
+                        // console.log(data.items[i].snippet.title);
+                        $scope.suggestions.push(data.items[i].snippet.title);
+                    }
+                }).error(function () {
+                    console.log("echonest query failed in getVideo");
+                });
+            }, 200);
         }
-        console.log("duplicate video found");
-      }
+    };
 
-      $scope.history.push($scope.code);
-      $scope.getNextVideo();
-    }).error(function() {
-      console.log("echonest query failed in getVideo");
-    });
-  };
+    $scope.getRelatedSong = function (songId) {
+        parameters = {
+            'song_id': songId,
+            'format': 'json',
+            'results': '20',
+            'type': 'song-radio',
+        };
 
-  $scope.getNextVideo = function() {
-    var requestUrl = YOUTUBE_API_URL + "?part=id&relatedToVideoId=" + $scope.code + '&order=relevance&type=video&key=' + YOUTUBE_API_KEY;
-    $http.get(requestUrl).success(function(data, status, headers, config) {
-      $scope.next = data.items[Math.floor(Math.random() * data.items.length)].id.videoId;
-      while ($scope.history.indexOf($scope.next) != -1) {
-        $scope.next = data.items[Math.floor(Math.random() * data.items.length)].id.videoId;
-      }
-    }).error(function() {
-      console.log("echonest query failed in getVideo");
-    });
-  };
+        if (songId) {
+            // $scope.queryEchonest('playlist', 'static', parameters, function(data, status, headers, config) {
+            //     debugger;
+            // });
+            console.log('getting related song');
+        }
+    };
 
-  $scope.skipVideo = function() {
-    $scope.code = $scope.next;
-    $scope.getNextVideo();
-  };
+    $scope.getVideo = function (searchTerm) {
+        var requestUrl = YOUTUBE_API_URL + "?part=snippet&q=" + encodeURIComponent(searchTerm) + 
+            '&videoEmbeddable=true&order=relevance&type=video&key=' + YOUTUBE_API_KEY;
+        $scope.currentSearchTerm = searchTerm;
+        $http.get(requestUrl).success(function (data, status, headers, config) {
+            for (var i = 0; i < data.items.length; i++) {
+                if ($scope.history.indexOf(data.items[i].id.videoId) == -1) {
+                    $scope.currentYoutubeId = data.items[i].id.videoId;
+                    $scope.currentYoutubeTitle = data.items[i].snippet.title;
+                    break;
+                }
+                console.log("duplicate video found");
+            }
 
-  $scope.setRandomVideo = function() {
-    $scope.selected = DEFAULT_SONGS[Math.floor(Math.random() * DEFAULT_SONGS.length)];
-    $scope.code = $scope.getVideo($scope.selected);
-  };
+            $scope.history.push($scope.currentYoutubeId);
+            $scope.getNextVideo();
+        }).error(function () {
+            console.log("echonest query failed in getVideo");
+        });
+    };
 
-  $scope.selected = DEFAULT_SONGS[Math.floor(Math.random() * DEFAULT_SONGS.length)];
+    $scope.getNextVideo = function () {
+        var requestUrl = YOUTUBE_API_URL + "?part=snippet&relatedToVideoId=" + $scope.currentYoutubeId + '&order=relevance&type=video&key=' + YOUTUBE_API_KEY;
+        $http.get(requestUrl).success(function (data, status, headers, config) {
+            $scope.nextYoutubeId = null;
+            console.log(data.items)
+            while (!$scope.nextYoutubeId || $scope.history.indexOf($scope.nextYoutubeId) != -1) {
+                var randomIndex = Math.floor(Math.random() * data.items.length);
+                $scope.nextYoutubeId = data.items[randomIndex].id.videoId;
+                $scope.nextYoutubeTitle = data.items[randomIndex].snippet.title;
+            }
+        }).error(function () {
+            console.log("echonest query failed in getVideo");
+        });
+    };
 
-  console.log('current selected: ' + $scope.selected);
+    $scope.skipVideo = function () {
+        console.log("skipping current video");
+        $scope.currentYoutubeId = $scope.nextYoutubeId;
+        $scope.currentYoutubeTitle = $scope.nextYoutubeTitle;
+        $scope.getNextVideo();
+    };
 
-  $scope.code = $scope.getVideo($scope.selected);
+    $scope.setRandomVideo = function () {
+        console.log("getting random video");
+        $scope.currentSearchTerm = DEFAULT_SONGS[Math.floor(Math.random() * DEFAULT_SONGS.length)];
+        $scope.getVideo($scope.currentSearchTerm);
+    };
 
-  $scope.next = '';
+    $scope.currentSearchTerm = '';
 
-  $scope.suggestions = [];
+    console.log('current selected: ' + $scope.currentSearchTerm);
 
-  $scope.history = []
+    $scope.currentYoutubeTitle = '';
+    $scope.currentYoutubeId = '';
+
+    $scope.nextYoutubeId = '';
+    $scope.nextYoutubeTitle = '';
+
+    $scope.suggestions = [];
+
+    $scope.history = []
+
+    $scope.setRandomVideo();
 });
--- playalista/static/youtube.js
+++ playalista/static/youtube.js
@@ -1,55 +1,58 @@
 var app = angular.module('youtube', []);
 
 app.run(function () {
-  var tag = document.createElement('script');
-  tag.src = "https://www.youtube.com/iframe_api";
-  var firstScriptTag = document.getElementsByTagName('script')[0];
-  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    var tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 });
 
-app.directive('youtube', ['$window', function($window, $sce) {
+app.directive('youtube', ['$window',
+    function ($window, $sce) {
 
-  return {
-    restrict: 'EA',
-    scope: {
-      code: '=',
-      onPlayFinish: "="
-    },
-    replace: true,
-    template: '<div style="height:500px;" id="player"></div>',
-    link: function (scope) {
-      var player;
-      scope.$watch('code', function (newVal) {
-        if (newVal) {
-          if (!player) {
-            player = new YT.Player('player', {
-              height: '100%',
-              width: '80%',
-              videoId: 'M71c1UVf-VE',
-              playerVars: {
-                wmode: "transparent",
-                autoplay: "1",
-              },
-              events: {
-                'onReady': function() {
-                  player.loadVideoById(newVal);
-                },
-                'onStateChange': onStateChange,
-              }
-            })
-          }
-          else {
-            player.loadVideoById(newVal);
-          }
-        }
-      });
+        return {
+            restrict: 'EA',
+            scope: {
+                code: '=',
+                onPlayFinish: "="
+            },
+            replace: true,
+            template: '<div style="height:100%;" id="player"></div>',
+            link: function (scope) {
+                var player;
+                scope.$watch('code', function (newVal) {
+                    if (newVal) {
+                        if (!player) {
+                            setTimeout(function(){
+                                player = new YT.Player('player', {
+                                    height: '100%',
+                                    width: '80%',
+                                    videoId: 'M71c1UVf-VE',
+                                    playerVars: {
+                                        wmode: "transparent",
+                                        autoplay: "1",
+                                    },
+                                    events: {
+                                        'onReady': function () {
+                                            player.loadVideoById(newVal);
+                                        },
+                                        'onStateChange': onStateChange,
+                                    }
+                                })
+                            }, 100);
+                        } else {
+                            player.loadVideoById(newVal);
+                        }
+                    }
+                });
 
-      function onStateChange(state) {
-        if (state.data == 0) {
-          scope.onPlayFinish();
-        }
-      }
+                function onStateChange(state) {
+                    if (state.data == 0) {
+                        scope.onPlayFinish();
+                    }
+                }
+            }
+        };
     }
-  };
-}]);
+]);
